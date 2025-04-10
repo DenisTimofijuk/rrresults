4ca5492db289e6e165454633ca2abbb8e3e9b985
Denis <denis.timofijuk@gmail.com>
2025-04-10 17:11:12 +0300

    api manager updated and prepared for production.

--- src/utils/apisManager.ts
+++ src/utils/apisManager.ts
@@ -1,13 +1,12 @@
-import MockReviewApiService from "../data/mock/mock-api-service";
 import { getJSONData } from "../data/getData";
 import { RequestCategoriesResult } from "../types/categories.type";
 import { ExperResultData } from "../types/ExpertTableData.type";
 import { ResultData } from "../types/Rezults.type";
 
-const mockApiService = new MockReviewApiService({
-  errorRate: 0.15,
-  verboseLogging: true // Enable for development
-});
+// const mockApiService = new MockReviewApiService({
+//   errorRate: 0.15,
+//   verboseLogging: true // Enable for development
+// });
 
 const apiManager = {
   getObservations: (selectedCategory: string) => getJSONData<ExperResultData[]>(`./mock/observations.json?c=${selectedCategory}`),
@@ -17,18 +16,23 @@ const apiManager = {
   savePointsForObservations: (data: Array<{
     observation_id: number;
     points: number;
-  }>) => {
-    // TODO: refactor to actual fetching endpoint with expected format.
-    console.log('Posting as stringified JSON:', JSON.stringify(data));
-    return mockApiService.saveRowReview(0, data)
-  },
+  }>) => fetch('./endpoint-to-save-points-for-observations', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  }),
   saveReviewComment: (data: {
     taxonID: number;
     comment: string
-  }) => {
-    // TODO: refactor to actual fetching endpoint with expected format.
-    return mockApiService.saveRowReview(0, data)
-  }
+  }) => fetch('./endpoint-to-save-comment', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
 };
 
 export default apiManager;
