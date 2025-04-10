21756aaa3e2eb27c3b9381f3581ba27ef3b17778
Denis <denis.timofijuk@gmail.com>
2025-04-10 15:06:50 +0300

    api manager updated

--- src/utils/apisManager.ts
+++ src/utils/apisManager.ts
@@ -5,23 +5,27 @@ import { ExperResultData } from "../types/ExpertTableData.type";
 import { ResultData } from "../types/Rezults.type";
 
 const mockApiService = new MockReviewApiService({
-    errorRate: 0.15,
-    verboseLogging: true // Enable for development
-  });
+  errorRate: 0.15,
+  verboseLogging: true // Enable for development
+});
 
 const apiManager = {
-    getObservations: (selectedCategory: string) => getJSONData<ExperResultData[]>(`./mock/observations.json?c=${selectedCategory}`),
-    getAvailableYears: () => getJSONData<number[]>('./mock/years.json'),
-    getAvailableCategories: () => getJSONData<RequestCategoriesResult>(`./mock/categories.json`),
-    getResultsByYear: (selectedYear: string) => getJSONData<ResultData>(`./mock/results.json?y=${selectedYear}`),
-    savePointsForObservations: (data: Array<{
-      observation_id: number;
-      points: number;
+  getObservations: (selectedCategory: string) => getJSONData<ExperResultData[]>(`./mock/observations.json?c=${selectedCategory}`),
+  getAvailableYears: () => getJSONData<number[]>('./mock/years.json'),
+  getAvailableCategories: () => getJSONData<RequestCategoriesResult>(`./mock/categories.json`),
+  getResultsByYear: (selectedYear: string) => getJSONData<ResultData>(`./mock/results.json?y=${selectedYear}`),
+  savePointsForObservations: (data: Array<{
+    observation_id: number;
+    points: number;
   }>) => {
     // TODO: refactor to actual fetching endpoint with expected format.
     console.log('Posting as stringified JSON:', JSON.stringify(data));
     return mockApiService.saveRowReview(0, data)
   },
+  savereviewComment: (taxonID: number, comment: string) => { 
+    // TODO: refactor to actual fetching endpoint with expected format.
+    return mockApiService.saveRowReview(taxonID, {comment})
+  }
 };
 
 export default apiManager;
